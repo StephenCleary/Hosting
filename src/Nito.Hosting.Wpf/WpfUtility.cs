3467aafa4500b82121ccbd6df9855e486da911cb
Stephen Cleary <github.ourteddybear@xoxy.net>
2024-01-28 22:44:10 -0500

    More comments.

--- src/Nito.Hosting.Wpf/WpfUtility.cs
+++ src/Nito.Hosting.Wpf/WpfUtility.cs
@@ -5,14 +5,21 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Nito.Hosting.Wpf;
 
 /// <summary>
-/// Provides utilities for markup-related types.
+/// Provides utilities for WPF types.
 /// </summary>
 public static class WpfUtility
 {
 	/// <summary>
 	/// Returns a factory method for the specified type that will call <see cref="IComponentConnector.InitializeComponent"/> after creating the instance.
 	/// </summary>
-	/// <typeparam name="T">The markup type to be created.</typeparam>
+	/// <remarks>
+	///	<para>Most markup-related types invoke <c>InitializeComponent</c> from their constructor.</para>
+	/// <para>For some completely unknown reason, WPF application components normally do <em>not</em>. For those components, you must explicitly call <c>InitializeComponent</c> after construction.</para>
+	/// <para>Who knows why. It's just an annoying inconsistency.</para>
+	/// <para>Naturally, <c>InitializeComponent</c> must be called. And cannot be called twice. And there's no way to tell whether it's been called.</para>
+	/// <para>Sigh.</para>
+	/// </remarks>
+	/// <typeparam name="T">The WPF application type to be created.</typeparam>
 #if NET472
 	public static Func<IServiceProvider, T> CreateApplicationInstance<T>()
 #else
